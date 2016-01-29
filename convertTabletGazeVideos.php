b30a262c45fdaa96179c3c3f684ad3f84d0f5450
Manuel Martin <manuel.martin@wearebase.com>
2016-01-29 14:49:32 +0000

    Update convertTabletGazeVideos.php

--- convertTabletGazeVideos.php
+++ convertTabletGazeVideos.php
@@ -9,11 +9,10 @@ $poses = range(1, 4);
 echo "#!/bin/bash\n";
 
 foreach ($poses as $p) {
-    $file = fopen("~/gaze-detection/TabletGaze/start_times/pose$p.csv", "r");
-    $subject = 1;
-    while (($data = fgetcsv($file)) !== false) {
+    $data = array_map('str_getcsv', file("~/gaze-detection/TabletGaze/start_times/pose$p.csv"));
+    foreach ($subjects as $subject) {
         foreach ($trials as $t) {
-            $startTime = sprintf('%2.3f', (float) $data[$t - 1]);
+            $startTime = sprintf('%2.3f', (float) $data[$subject - 1][$t - 1]);
             $startTime = str_pad($startTime, 6, '0', STR_PAD_LEFT);
             $baseName = sprintf('%d_%d_%d', $subject, $t, $p);
             $commands = [];
@@ -21,23 +20,25 @@ foreach ($poses as $p) {
             $commands[] = "avconv -i $subject/$baseName.mp4 -ss 00:00:$startTime -c:v copy -an $subject/$baseName.fix.mp4";
             $commands[] = "avconv -i $subject/$baseName.fix.mp4 -t 00:01:45 -c:v copy -an $subject/$baseName.new.mp4";
             $commands[] = "avconv -i $subject/$baseName.new.mp4 -r 25 $subject/$baseName.new.25fps.mp4";
-            $commands[] = "$featureExtractor -rigid -verbose -f \"$subject/$baseName.new.25fps.mp4\" -ogaze \"$subject/$baseName.csv\"";
-            $commands[] = "head -2626 $subject/$baseName.csv > $subject/$baseName.csv";
-            $commands[] = "cut -d, -f4-15 $subject/$baseName.csv > $subject/$baseName.vectors.csv";
+            $commands[] = "$featureExtractor -q -rigid -verbose -f \"$subject/$baseName.new.25fps.mp4\" -ogaze \"$subject/$baseName.csv\"";
+            $commands[] = "head -2626 $subject/$baseName.csv > $subject/$baseName.tmp";
+            $commands[] = "cut -d, -f4-15 $subject/$baseName.tmp > $subject/$baseName.vectors.csv";
             $commands[] = "paste -d, $subject/$baseName.vectors.csv <(cut -d, -f1 TabletGazePoints.csv) > $subject/$baseName.x.csv";
             $commands[] = "paste -d, $subject/$baseName.vectors.csv <(cut -d, -f2 TabletGazePoints.csv) > $subject/$baseName.y.csv";
-            echo implode(' && ', $commands) . "\n";
+            $commands[] = "rm $subject/$baseName.vectors.csv";
+            $commands[] = "rm $subject/$baseName.tmp";
+            echo implode(' && ', $commands)."\n";
         }
-        $subject++;
     }
 }
 
-foreach($subjects as $subject) {
-    echo "nawk 'FNR==1 && NR!=1{next;}{print}' $subject/*.x.csv >> all.x.csv\n";
-    echo "nawk 'FNR==1 && NR!=1{next;}{print}' $subject/*.y.csv >> all.y.csv\n";
-}
+foreach ($subjects as $subject) {
+    echo "nawk 'FNR==1 && NR!=1{next;}{print}' $subject/*.x.csv > $subject/$subject.x.tmp && mv $subject/$subject.x.tmp $subject/$subject.x.csv\n";
+    echo "nawk 'FNR==1 && NR!=1{next;}{print}' $subject/*.y.csv > $subject/$subject.y.tmp && mv $subject/$subject.y.tmp $subject/$subject.y.csv\n";
 
-echo "java -cp $wekaJar weka.core.converters.CSVLoader all.x.csv > all.x.arff\n";
-echo "java -cp $wekaJar weka.core.converters.CSVLoader all.y.csv > all.y.arff\n";
-echo "java -cp $wekaJar weka.classifiers.lazy.IBk -t all.x.arff -classifications \"weka.classifiers.evaluation.output.prediction.CSV\" -p 0 > predictions.x.csv\n";
-echo "java -cp $wekaJar weka.classifiers.lazy.IBk -t all.y.arff -classifications \"weka.classifiers.evaluation.output.prediction.CSV\" -p 0 > predictions.y.csv\n";
+    echo "sed '/^,/d' $subject/$subject.x.csv > $subject/$subject.x.tmp && mv $subject/$subject.x.tmp $subject/$subject.x.csv\n";
+    echo "sed '/^,/d' $subject/$subject.y.csv > $subject/$subject.y.tmp && mv $subject/$subject.y.tmp $subject/$subject.y.csv\n";
+
+    echo "java -cp $wekaJar weka.core.converters.CSVLoader -M \" -nan\" $subject/$subject.x.csv > $subject/$subject.x.arff\n";
+    echo "java -cp $wekaJar weka.core.converters.CSVLoader -M \" -nan\" $subject/$subject.y.csv > $subject/$subject.y.arff\n";
+}
